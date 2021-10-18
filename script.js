5159c8e8a694e04634de94ca7cf108277ff0848a
CMUC <88113256+CMUC@users.noreply.github.com>
2021-10-18 16:27:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let name = 'Claudia';
+console.log(name);
+
+let firstName = 'Gegam';
+let lastName = firstName + 'Soghomonyan';
+console.log(lastName);
+
+let number1 = 6;
+let number2 = number1 * 4;
+console.log(number2);
+
+let number3 = 6;
+let number4 = number3 / 4;
+console.log(number4);
+
+let number5 = 6;
+let number6 = number5 % 4;
+console.log(number6);
+
+let age = 'twentyseven';
+console.log(age);
